df200d2f0c4fbbfeacec93f76a75d26ccc3a82f3
Elijah Kauba <elijahkauba@gmail.com>
2023-10-10 20:39:51 -0400

    Wrote some sample code fixed assign1

---
+++ Eli/MyFirstScript.py
@@ -0,0 +1,10 @@
+print("Welcome")
+print()
+
+nameFirst=input("What is your First name? ")
+nameLast=input("What is your Last name? ")
+
+fullName=nameFirst+" "+nameLast
+
+print("Hello: "+fullName)
+print()
