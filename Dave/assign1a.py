7d3b249b7905678a3c7494b072254c004c872ca7
Dave Wasserman <dave@wassermail.com>
2023-10-10 21:04:48 -0400

    added initial code

---
+++ Dave/assign1a.py
@@ -0,0 +1,11 @@
+# Write a python code that asks a user to enter two integers and
+# multiply them together. Print the answer
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Multiply the two integers
+result = num1 * num2
+
+# Print the answer
+print("The result of multiplying", num1, "and", num2, "is:", result)
