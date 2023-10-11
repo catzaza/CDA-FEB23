bca8efadf20e654d78b523f2b66345d64f00c4a9
Dave Wasserman <dave@wassermail.com>
2023-10-10 22:03:59 -0400

    code for assignment 1d

---
+++ Dave/assign1d.py
@@ -0,0 +1,28 @@
+# Write a python input program that will ask new employees for their username, password, and email.
+# Print the output.
+
+# Ask the new employee for their information
+print()
+print("When prompted below, please enter your name, username, password, and email.")
+print()
+namefull = input("Enter your first and last name (ex: Bob Smith): ")
+print()
+username = input("Enter your username: ")
+print()
+password = input("Enter your password: ")
+print()
+email = input("Enter your email address: ")
+print()
+print()
+print()
+print()
+
+# Print the collected information
+print("NEW EMPLOYEE INFORMATON:")
+print()
+print("Employee:", namefull)
+print("Username:", username)
+print("Password:", password)
+print("Email:", email)
+print()
+print()
