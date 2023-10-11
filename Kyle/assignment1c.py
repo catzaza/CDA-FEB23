8d89d8410962e6a9c6f91017adcc3ee35e39263d
kylestuller <kylestuller@proton.me>
2023-10-11 12:12:13 +0700

    Changes

--- Kyle/assignment1c.py
+++ Kyle/assignment1c.py
@@ -7,10 +7,10 @@ for i in range(3):
         ip = input(f"Enter IP address {i + 1}: ")
         try:
             # Check if the entered IP address is valid
-            ip_obj = ipaddress.IPv4Address(ip)
+            ip_obj = ip_address.IPv4Address(ip)
             valid_ip_addresses.append(str(ip_obj))  # Convert to string to store in the list
             break  # Exit the loop if a valid IP is provided
-        except ipaddress.AddressValueError:
+        except ip_address.AddressValueError:
             print("Invalid IP address. Please try again.")
 
 # Display the gathered valid IP addresses
