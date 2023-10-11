f3e1d41bbe03f1187f795b1f5a6dfe23f65f7360
kylestuller <kylestuller@proton.me>
2023-10-11 11:34:55 +0700

    Assignemnts 1a, 1b

---
+++ Kyle/assignment1a.py
@@ -0,0 +1,6 @@
+num1=int(input("Number 1: "))
+num2=int(input("Number 2: "))
+
+answer=num1*num2
+
+print(answer)
---
+++ Kyle/assignment1b.py
@@ -0,0 +1,11 @@
+# This assignment will ask a customer about what they would like to purchase
+
+print('What would you like to purchase, dear sir/madame?')
+
+item_1=input("Item 1: ")
+item_2=input("Item 2: ")
+item_3=input("Item 3: ")
+item_4=input("Item 4: ")
+item_5=input("Item 5: ")
+
+print("Great! We are ready to checkout for the following items: item_1, item_2, item_3, item_4, item_5")
