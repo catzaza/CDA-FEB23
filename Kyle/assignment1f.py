c9f97c7be358eaf6d2fdd1b3ded59de6aac43c34
kylestuller <kylestuller@proton.me>
2023-10-12 22:13:06 +0700

    Here is assignment 1f

---
+++ Kyle/assignment1f.py
@@ -0,0 +1,8 @@
+# For the user to input the projected total sales
+total_sales = float(input("Enter the projected amount of total sales: "))
+
+# Calculate the profit
+profit = total_sales * 0.23
+
+# Display the calculated profit
+print("The profit from projected sales is:", profit)
