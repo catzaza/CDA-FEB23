6646aefae956e889dae6f70ff96b0388cbc58b64
kylestuller <kylestuller@proton.me>
2023-10-12 22:09:25 +0700

    Here is assignment 1e

---
+++ Kyle/assignment1e.py
@@ -0,0 +1,13 @@
+# This will be a program that displays my name, address, phone number, and dream major. 
+
+# To display the different fields
+name = input("What is your name? ")
+address = input("What is your full address? ")
+phone_number = input("What is your phone number? ")
+dream_job = input("What is your dream job? ")
+
+print("Here are the answers to the questions:")
+print("Name:", name)
+print("Address:", address)
+print("Phone Number:", phone_number)
+print("Dream Job:", dream_job)
