aff52323e14e29507866d72fdb072b9d311693e4
Paul Lehto <lehtopa07@gmail.com>
2023-10-16 13:24:24 -0400

    Paul Assign1f

---
+++ Paul/Paul Assign1f.py
@@ -0,0 +1,5 @@
+# code to allow user to input company sales annually, and calculate the percentage of profit made
+
+total_sales = float(input("Enter projected annual sales: "))
+profit = float(total_sales * 0.23)
+print("Projected annual profits: " , profit)
