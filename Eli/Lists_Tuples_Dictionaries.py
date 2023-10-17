768629395f4a1c8a57b34e0e68c45ce7038c040f
Elijah Kauba <elijahkauba@gmail.com>
2023-10-16 20:06:11 -0400

    Python Data Containers Notes

---
+++ Eli/Lists_Tuples_Dictionaries.py
@@ -0,0 +1,52 @@
+### Lists
+
+#name=[["elijah", "sandellyo", "kauba"], [1,2,3]]
+#name[0][1]
+#
+#
+#while 1==1:
+#  name[0]=int(input("Enter First Name :"))
+#  name[1]=int(input("Enter Middle Name :"))
+#  name[2]=input("Enter Last Name :")
+#  print(name)
+#
+#  num=name[0]+name[1]
+#  print(num)
+
+
+### Tuples
+#
+#name=("elijah", "sandellyo", "kauba")
+#
+#print(name[1])
+#
+#name[3]="test"
+
+
+### Dictionaires
+#
+#name={ "first_name":"elijah", "mid_name":"sandellyo", "last_name":"kauba" }
+#
+#name["sir_name"]="Mr"
+#
+#print(name["sir_name"], name["first_name"])
+
+
+### Sets
+
+#myset = set()
+
+#myset = set(['a', 'b', 'c'])
+#myset= set("abc")
+#myset= set("aabbbc")
+
+#myset.add("a")
+#myset.add("b")
+#myset.add("c")
+#
+#myset.update(["d", "e", "f"])
+#myset.discard("g")
+#
+#myset.clear()
+#
+#print(myset)
